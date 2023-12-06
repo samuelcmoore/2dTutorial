9fe1d51d132538d7f596a2427dd12170640f59bd
samuelcmoore <smoore442@ivytech.edu>
2023-12-06 10:51:14 -0500

    M06 update

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -8,6 +8,8 @@ using UnityEngine;
     public bool vertical;
     public float changeTime = 3.0f;
 
+    public ParticleSystem smokeEffect;
+
     Rigidbody2D rigidbody2D;
     float timer;
     int direction = 1;
@@ -83,5 +85,6 @@ using UnityEngine;
         rigidbody2D.simulated = false;
         //optional if you added the fixed animation
         animator.SetTrigger("Fixed");
+        smokeEffect.Stop();
     }
 }
