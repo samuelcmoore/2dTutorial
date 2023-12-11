65460d245b43875e627e76128cf58a9912433992
samuelcmoore <smoore442@ivytech.edu>
2023-12-11 16:02:29 -0500

    M07

--- Assets/Scripts/HealthCollectible.cs
+++ Assets/Scripts/HealthCollectible.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class HealthCollectible : MonoBehaviour
 {
+    public AudioClip collectedClip;
+    
     void OnTriggerEnter2D(Collider2D other)
     {
         RubyController controller = other.GetComponent<RubyController>();
 
         if (controller != null)
         {
-            if(controller.currentHealth < controller.maxHealth)
+            if (controller.health < controller.maxHealth)
             {
                 controller.ChangeHealth(1);
                 Destroy(gameObject);
+            
+                controller.PlaySound(collectedClip);
             }
         }
+
     }
 }
--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -10,6 +10,9 @@ public class RubyController : MonoBehaviour
     
     public GameObject projectilePrefab;
     
+    public AudioClip throwSound;
+    public AudioClip hitSound;
+    
     public int health { get { return currentHealth; }}
     public int currentHealth;
     
@@ -24,6 +27,8 @@ public class RubyController : MonoBehaviour
     Animator animator;
     Vector2 lookDirection = new Vector2(1,0);
     
+    AudioSource audioSource;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -31,6 +36,8 @@ public class RubyController : MonoBehaviour
         animator = GetComponent<Animator>();
         
         currentHealth = maxHealth;
+
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -62,6 +69,19 @@ public class RubyController : MonoBehaviour
         {
             Launch();
         }
+        
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+            if (hit.collider != null)
+            {
+                NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
+                if (character != null)
+                {
+                    character.DisplayDialog();
+                }
+            }
+        }
     }
     
     void FixedUpdate()
@@ -82,9 +102,12 @@ public class RubyController : MonoBehaviour
             
             isInvincible = true;
             invincibleTimer = timeInvincible;
+            
+            PlaySound(hitSound);
         }
         
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        
         UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
     }
     
@@ -96,5 +119,12 @@ public class RubyController : MonoBehaviour
         projectile.Launch(lookDirection, 300);
 
         animator.SetTrigger("Launch");
+        
+        PlaySound(throwSound);
+    } 
+    
+    public void PlaySound(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
     }
 }
